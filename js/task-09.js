2fa4fdc63a1075d5e4e883b6b5bca6b2d992a6a8
Victor Nikolenko <victor1982nik@gmail.com>
2022-08-20 15:26:10 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const btnChangeColorRef = document.querySelector('button.change-color');
+const spanColorRef = document.querySelector('.color');
+const bodyRef =  document.querySelector('body');
+
+btnChangeColorRef.addEventListener('click', OnBtnClickHaldler);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function OnBtnClickHaldler() {
+  const color = getRandomHexColor();  
+
+  bodyRef.style.backgroundColor = color;
+  spanColorRef.textContent = color;
 }
