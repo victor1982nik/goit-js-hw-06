ae0ae016bd8701025e6eeb7809a7715e90f86f7a
Victor Nikolenko <victor1982nik@gmail.com>
2022-08-20 15:26:08 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener('submit', OnFormSubmit)
+
+function OnFormSubmit(event) {
+    event.preventDefault();
+
+    const { currentTarget } = event;
+    const { email, password } = currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert("Логин, пароль не может быть пустой строкой");
+    }
+        
+    const user = {
+        email: email.value,
+        password: password.value,
+    };
+        
+    currentTarget.reset();
+    console.log(user);
+   // return user;
+}
+
 
